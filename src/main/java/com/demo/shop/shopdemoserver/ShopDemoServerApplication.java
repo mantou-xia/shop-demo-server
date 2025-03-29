8aa4f99c4d070345a72265005ae1651cbafb1d58
mantouxia <3775060926@qq.com>
2025-03-29 14:50:15 +0800

    test

---
+++ src/main/java/com/demo/shop/shopdemoserver/ShopDemoServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.shop.shopdemoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDemoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopDemoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/shop/shopdemoserver/ShopDemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.shop.shopdemoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopDemoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
